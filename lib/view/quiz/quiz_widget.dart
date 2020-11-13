6724ada822197d8367e810ba72eade6ef098a4aa
Nadar <devasahayam-michaelanthony.nadar@capgemini.com>
2020-11-13 14:47:50 +0530

    added animation

--- lib/view/quiz/quiz_widget.dart
+++ lib/view/quiz/quiz_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:TataEdgeDemo/blocs/quiz/quiz_bloc.dart';
 import 'package:TataEdgeDemo/blocs/quiz/quiz_event.dart';
 import 'package:TataEdgeDemo/blocs/quiz/quiz_state.dart';
@@ -22,7 +24,7 @@ class QuizWidget extends StatelessWidget {
             builder: (context, state) {
               debugPrint(" the state is $state");
               if (state is ShowQuestion) {
-                return QuestionWidget(state.questions);
+                return QuestionWidget(state.questions,ValueKey(state.questions.question));
               } else if (state is QuizComplete) {
                 return QuizComplete();
               } else {
@@ -39,7 +41,7 @@ class QuizWidget extends StatelessWidget {
 class QuestionWidget extends StatelessWidget {
   Questions questions;
 
-  QuestionWidget(this.questions);
+  QuestionWidget(this.questions,Key key):super(key:key);
 
   @override
   Widget build(BuildContext context) {
@@ -108,26 +110,80 @@ class QuizComplete extends StatelessWidget {
   }
 }
 
-class QuestionTimeOut extends StatelessWidget {
+final Animatable<int> _kStepTween = StepTween(begin: 10, end: 1);
+
+class QuestionTimeOut extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return QuestionTimeOutState();
+  }
+}
+
+class QuestionTimeOutState extends State<QuestionTimeOut>
+    with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      lowerBound: 0.0,
+      upperBound: 2.0,
+      vsync: this,
+    )..forward();
+    // controller.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(16.0),
-      height: 120,
-      width: 120,
-      child: CustomPaint(
-        painter: ProgressPainter(),
-      ),
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (BuildContext context, Widget child) {
+        debugPrint(
+            " i was called and the value is ${controller.value}");
+        return Container(
+          margin: EdgeInsets.all(16.0),
+          height: 120,
+          width: 120,
+          child: CustomPaint(
+              painter: ProgressPainter(controller.value)),
+        );
+      },
     );
   }
 }
 
 class ProgressPainter extends CustomPainter {
+  double arcValue = 2;
+  int _arcConstant = 2;
+
+  // double _twoPi = math.pi * arcValue;
+  // double _epsilon = 0;
+  // // Canvas.drawArc(r, 0, 2*PI) doesn't draw anything, so just get close.
+  // double _sweep = _twoPi - _epsilon;
+  static const double startAngle = 270 * (math.pi / 180);
+
+  ProgressPainter(this.arcValue);
+
   @override
   void paint(Canvas canvas, Size size) {
+    // double _sweep = math.pi * _arcConstant/arcValue;
+    double value = arcValue == 0 ? 2 : _arcConstant - arcValue;//(_arcConstant - (2 / arcValue));
+    debugPrint(" the arc value is $value");
+    double _sweep = -(math.pi * value);
 
     var paint = Paint()
-      ..color = Colors.teal
+      ..color = Color(0xFFc034eb)
       ..strokeWidth = 2
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
@@ -136,17 +192,16 @@ class ProgressPainter extends CustomPainter {
 
     canvas.drawCircle(center, 60, paint);
     var paintRed = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 5
+      ..color = new Color(0xffe23535)
+      ..strokeWidth = 8
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
 
-
-    canvas.drawCircle(center, 60, paintRed);
+    canvas.drawArc(Offset.zero & size, startAngle, _sweep, false, paintRed);
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+  bool shouldRepaint(ProgressPainter oldDelegate) {
+    return oldDelegate.arcValue != arcValue;
   }
 }
